8697af66ce4c1138438f3c3749bfb39f6a426513
Yohei Yasukawa <yohei@yasslab.jp>
2023-04-08 14:54:26 +0000

    Exec: rails generate scaffold User name:string email:string
    Exec: rails generate scaffold Micropost content:text user_id:integer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  # /users     -> users#index
+  # /users/new -> users#new
+  # /users/:id -> users#show
+  # ...
   root "hello#index"
 end
